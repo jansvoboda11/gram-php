8d4eb9c339db6d5f437dc09d041c78ae82879875
Jan Svoboda <jansvoboda11@gmail.com>
2017-02-20 12:45:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gram PHP
+
+PHP code generation driven by unit tests. Built using Gram library.
---
+++ src/main.cpp
@@ -0,0 +1,87 @@
+#include <fstream>
+#include <iostream>
+
+#include <gram/individual/crossover/OnePointCrossover.h>
+#include <gram/individual/mutation/NumberMutation.h>
+#include <gram/language/parser/BnfRuleParser.h>
+#include <gram/population/initializer/RandomInitializer.h>
+#include <gram/population/selector/TournamentSelector.h>
+#include <gram/util/bool_generator/TwisterBoolGenerator.h>
+#include <gram/util/number_generator/TwisterNumberGenerator.h>
+#include <gram/Evolution.h>
+
+using namespace gram;
+using namespace std;
+
+string execute(const string& command) {
+  FILE *pipe = popen(command.c_str(), "r");
+
+  if (!pipe) {
+    throw runtime_error("Could not open command line.");
+  }
+
+  string result;
+  char buffer[128];
+
+  while (fgets(buffer, 128, pipe)) {
+    result += buffer;
+  }
+
+  pclose(pipe);
+  return result;
+}
+
+class FakeEvaluator : public Evaluator {
+  double evaluate(string program) const {
+    // execute("phpunit")
+
+    return 0.0;
+  }
+};
+
+string loadFile(const string& name) {
+  ifstream grammarFile(name);
+
+  if (!grammarFile.is_open()) {
+    return "";
+  }
+
+  string content((istreambuf_iterator<char>(grammarFile)), istreambuf_iterator<char>());
+
+  return content;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    return 1;
+  }
+
+  string grammarString = loadFile(argv[1]);
+
+  unique_ptr<NumberGenerator> numberGenerator1 = make_unique<TwisterNumberGenerator>();
+  unique_ptr<NumberGenerator> numberGenerator2 = make_unique<TwisterNumberGenerator>();
+  unique_ptr<NumberGenerator> numberGenerator3 = make_unique<TwisterNumberGenerator>();
+  unique_ptr<NumberGenerator> numberGenerator4 = make_unique<TwisterNumberGenerator>();
+  unique_ptr<BoolGenerator> boolGenerator = make_unique<TwisterBoolGenerator>(1.0);
+
+  auto selector = make_unique<TournamentSelector>(move(numberGenerator1));
+  auto mutation = make_unique<NumberMutation>(move(boolGenerator), move(numberGenerator2));
+  auto crossover = make_unique<OnePointCrossover>(move(numberGenerator3));
+  auto reproducer = make_shared<Reproducer>(move(selector), move(crossover), move(mutation));
+
+  BnfRuleParser parser;
+
+  auto grammar = make_shared<ContextFreeGrammar>(parser.parse(grammarString));
+
+  RandomInitializer initializer(move(numberGenerator4), grammar, 50);
+
+  unique_ptr<Evaluator> evaluator = make_unique<FakeEvaluator>();
+
+  Evolution evolution(move(evaluator));
+
+  Population population = initializer.initialize(60, reproducer);
+
+//  Individual result = evolution.run(population);
+
+  return 0;
+}
