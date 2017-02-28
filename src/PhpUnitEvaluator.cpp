7a4cafdb1b8ed880b7905e5c3791d46f55f2464b
Jan Svoboda <jansvoboda11@gmail.com>
2017-02-28 22:55:18 +0100

    Add XML library

--- src/PhpUnitEvaluator.cpp
+++ src/PhpUnitEvaluator.cpp
@@ -1,8 +1,10 @@
 #include "../include/PhpUnitEvaluator.h"
 
 #include <fstream>
+#include <pugixml.hpp>
 #include <regex>
 
+using namespace pugi;
 using namespace std;
 
 PhpUnitEvaluator::PhpUnitEvaluator(CommandLine commandLine, const string& path)
@@ -25,6 +27,23 @@ double PhpUnitEvaluator::evaluate(string program) const {
 }
 
 double PhpUnitEvaluator::calculateFitness(string testOutput) const {
+  string documentPath(path + "phpunit-result.xml");
+
+  xml_document document;
+
+  if (!document.load_file(documentPath.c_str())) {
+    return 0.0;
+  }
+
+  string name = document
+      .child("testsuites")
+      .child("testsuite")
+      .child("testsuite")
+      .child("testcase")
+      .child("failure")
+      .child("expected")
+      .text()
+      .get(); // first failure, expected value
 
   return 0.0;
 }
