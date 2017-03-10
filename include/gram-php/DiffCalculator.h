aaabe33e3b971d3be0ceaa131a46a254f9908cae
Jan Svoboda <jansvoboda11@gmail.com>
2017-03-10 23:41:05 +0100

    Split diff into more methods

--- include/gram-php/DiffCalculator.h
+++ include/gram-php/DiffCalculator.h
@@ -7,6 +7,10 @@ class DiffCalculator {
  public:
   double calculate(PhpLiteral first, PhpLiteral second) const;
  private:
+  double calculate(bool first, bool second) const;
+  double calculate(int first, int second) const;
+  double calculate(double first, double second) const;
+  double calculate(std::string first, std::string second) const;
   static constexpr double TYPE_DIFF = 1000.0;
 };
 
--- src/DiffCalculator.cpp
+++ src/DiffCalculator.cpp
@@ -7,5 +7,37 @@ double DiffCalculator::calculate(PhpLiteral first, PhpLiteral second) const {
     return TYPE_DIFF;
   }
 
+  if (first.type() == PhpType::boolean()) {
+    return calculate(first.booleanValue(), second.booleanValue());
+  }
+
+  if (first.type() == PhpType::integer()) {
+    return calculate(first.integerValue(), second.integerValue());
+  }
+
+  if (first.type() == PhpType::decimal()) {
+    return calculate(first.decimalValue(), second.decimalValue());
+  }
+
+  if (first.type() == PhpType::string()) {
+    return calculate(first.stringValue(), second.stringValue());
+  }
+
+  return 0.0;
+}
+
+double DiffCalculator::calculate(bool first, bool second) const {
+  return 0.0;
+}
+
+double DiffCalculator::calculate(int first, int second) const {
+  return 0.0;
+}
+
+double DiffCalculator::calculate(double first, double second) const {
+  return 0.0;
+}
+
+double DiffCalculator::calculate(string first, string second) const {
   return 0.0;
 }
