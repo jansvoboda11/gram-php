13c69a0f38631cd773bdede75485b1c18e7eed43
Jan Svoboda <jansvoboda11@gmail.com>
2017-03-10 23:25:49 +0100

    Split unserialize into multiple methods

--- include/gram-php/PhpUnserializer.h
+++ include/gram-php/PhpUnserializer.h
@@ -8,6 +8,11 @@
 class PhpUnserializer {
  public:
   PhpLiteral unserialize(std::string serializedLiteral) const;
+ private:
+  PhpLiteral unserializeBoolean(std::string value) const;
+  PhpLiteral unserializeInteger(std::string value) const;
+  PhpLiteral unserializeDecimal(std::string value) const;
+  PhpLiteral unserializeString(std::string value, std::string promisedLength) const;
 };
 
 #endif // GRAM_PHP_UNSERIALIZER
--- src/PhpUnserializer.cpp
+++ src/PhpUnserializer.cpp
@@ -13,64 +13,70 @@ PhpLiteral PhpUnserializer::unserialize(string serializedLiteral) const {
   smatch matches;
 
   if (regex_match(serializedLiteral, matches, booleanPattern)) {
-    string match = matches[1];
-
-    if (match == "0") {
-      return PhpLiteral(false);
-    }
-
-    if (match == "1") {
-      return PhpLiteral(true);
-    }
-
-    throw logic_error("Could not unserialize boolean value \"" + match + "\".");
+    return unserializeBoolean(matches[1]);
   }
 
   if (regex_match(serializedLiteral, matches, integerPattern)) {
-    string match = matches[1];
-
-    try {
-      int value = stoi(match);
-      return PhpLiteral(value);
-    } catch (out_of_range exception) {
-      throw logic_error("Could not unserialize too large integer \"" + match + "\".");
-    } catch (invalid_argument exception) {
-      throw logic_error("Could not unserialize integer \"" + match + "\".");
-    }
+    return unserializeInteger(matches[1]);
   }
 
   if (regex_match(serializedLiteral, matches, decimalPattern)) {
-    string match = matches[1];
-
-    try {
-      double value = stod(match);
-      return PhpLiteral(value);
-    } catch (out_of_range exception) {
-      throw logic_error("Could not unserialize too large decimal number \"" + match + "\".");
-    } catch (invalid_argument exception) {
-      throw logic_error("Could not unserialize decimal number \"" + match + "\".");
-    }
+    return unserializeDecimal(matches[1]);
   }
 
   if (regex_match(serializedLiteral, matches, stringPattern)) {
-    string promisedLength = matches[1];
-    string value = matches[2];
-    int length;
-
-    try {
-      length = stoi(promisedLength);
-    } catch (out_of_range exception) {
-      throw logic_error("Could not unserialize too large string length \"" + promisedLength + "\".");
-    } catch (invalid_argument exception) {
-      throw logic_error("Could not unserialize string length \"" + promisedLength + "\".");
-    }
-
-    if (length != value.length()) {
-      throw logic_error("Length of string \"" + value + "\" does not match promised length \"" + promisedLength + "\".");
-    }
-
-    return PhpLiteral(value);
+    return unserializeString(matches[2], matches[1]);
   }
 
   throw logic_error("Could not recognize serialized literal.");
+}
+
+PhpLiteral PhpUnserializer::unserializeBoolean(string value) const {
+  if (value == "0") {
+    return PhpLiteral(false);
+  }
+
+  if (value == "1") {
+    return PhpLiteral(true);
+  }
+
+  throw logic_error("Could not unserialize boolean value \"" + value + "\".");
+}
+
+PhpLiteral PhpUnserializer::unserializeInteger(string value) const {
+  try {
+    return PhpLiteral(stoi(value));
+  } catch (out_of_range exception) {
+    throw logic_error("Could not unserialize too large integer \"" + value + "\".");
+  } catch (invalid_argument exception) {
+    throw logic_error("Could not unserialize integer \"" + value + "\".");
+  }
+}
+
+PhpLiteral PhpUnserializer::unserializeDecimal(string value) const {
+  try {
+    return PhpLiteral(stod(value));
+  } catch (out_of_range exception) {
+    throw logic_error("Could not unserialize too large decimal number \"" + value + "\".");
+  } catch (invalid_argument exception) {
+    throw logic_error("Could not unserialize decimal number \"" + value + "\".");
+  }
+}
+
+PhpLiteral PhpUnserializer::unserializeString(string value, string promisedLength) const {
+  int length;
+
+  try {
+    length = stoi(promisedLength);
+  } catch (out_of_range exception) {
+    throw logic_error("Could not unserialize too large string length \"" + promisedLength + "\".");
+  } catch (invalid_argument exception) {
+    throw logic_error("Could not unserialize string length \"" + promisedLength + "\".");
+  }
+
+  if (length != value.length()) {
+    throw logic_error("Length of string \"" + value + "\" does not match promised length \"" + promisedLength + "\".");
+  }
+
+  return PhpLiteral(value);
 }
