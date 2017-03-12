0b27695ce9372a0c787f86611dedeee42d088ea1
Jan Svoboda <jansvoboda11@gmail.com>
2017-03-12 14:32:08 +0100

    Rename generated function

--- example/test/src/CalculatorTest.php
+++ example/test/src/CalculatorTest.php
@@ -7,21 +7,21 @@ class CalculatorTest extends TestCase
 {
     public function test_it_increments_zero()
     {
-        self::assertEquals(1, Calculator::inc(0));
+        self::assertEquals(1, Calculator::increment(0));
     }
 
     public function test_it_increments_one()
     {
-        self::assertEquals(2, Calculator::inc(1));
+        self::assertEquals(2, Calculator::increment(1));
     }
 
     public function test_it_increments_two()
     {
-        self::assertEquals(3, Calculator::inc(2));
+        self::assertEquals(3, Calculator::increment(2));
     }
 
     public function test_it_produces_the_answer_to_the_ultimate_question_of_life_the_universe_and_everything()
     {
-        self::assertEquals(42, Calculator::inc(41));
+        self::assertEquals(42, Calculator::increment(41));
     }
 }
